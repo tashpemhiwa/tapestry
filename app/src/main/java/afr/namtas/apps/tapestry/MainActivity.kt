278efa11b2375ae08809f1a066e368a7472c222d
Tashinga Pemhiwa <tashpemhiwa@gmail.com>
2025-12-09 21:25:49 +0200

    - Removed unused imports

--- app/src/main/java/afr/namtas/apps/tapestry/MainActivity.kt
+++ app/src/main/java/afr/namtas/apps/tapestry/MainActivity.kt
@@ -20,16 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.math.cos
-import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
